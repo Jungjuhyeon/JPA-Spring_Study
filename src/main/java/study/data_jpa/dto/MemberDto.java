c0eb54d7bd7ea024f1745f89604f2949d7dcc4ec
jungjoo <wjddis000507@gmail.com>
2024-07-27 22:02:04 +0900

    다양한 쿼리메소드 사용하기

---
+++ src/main/java/study/data_jpa/dto/MemberDto.java
@@ -0,0 +1,20 @@
+package study.data_jpa.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String team;
+
+
+    public MemberDto(Long id, String username, String team) {
+        this.id = id;
+        this.username = username;
+        this.team = team;
+    }
+
+
+}
--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
+import study.data_jpa.entity.Team;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -17,17 +21,107 @@ import static org.assertj.core.api.Assertions.*;
 @Transactional
 @Rollback(false)
 class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
-    public void  testMember(){
+    public void testMember() {
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
-        Member findMember = memberRepository.findById(savedMember.getId()).get();
+        Member findMember = memberRepository.findById(member.getId()).get();
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!!");
+    }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void testUsernameList() {
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for(String s:usernameList){
+            System.out.println("s = "+ s);
+        }
+    }
+
+    @Test
+    public void teamMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA",10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for(MemberDto dto:memberDto){
+            System.out.println("s = "+ dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for (Member s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aaa = memberRepository.findListByUsername("AAA");
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> aa = memberRepository.findOptionalByUsername("AAA");
+
+
+        //list를 조회할떄 빈 컬렉션을 제공해줌. list는 null 처리할 필요가 없다. 그냥 리스트는 절대 null이 안나옴. 무조건 빈 컬렉션을 리턴함 없어도.
+        //단건 조회같은 경우는 null을 반환해주기 때문에 null처리를 해줘야함.
+        //그래서 그냥 opitonal로 처리해!
     }
 }
